72a67e1f48fbf28736f3cbff9a859242daec21db
Sophie Wickham <sphwick@gmail.com>
2020-08-24 17:30:10 +0000

    Started product template

--- products/models.py
+++ products/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class Category(models.Model):
 
     class Meta: 
-        verbose_name_plural = 'Catergories'
-        
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
