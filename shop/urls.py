3aed23f490f93fda2852389c07c926389e7858d3
sw1ckham <sphwick@gmail.com>
2020-08-24 08:11:28 +0100

    Fixed typo on urls allauth not allauths

--- shop/urls.py
+++ shop/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauths.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
